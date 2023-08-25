b452c47932ee614f4d08d742329710491fd1cd31
huangr0867 <rhuang53@gmail.com>
2023-08-25 10:06:31 -0700

    edits

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { logo } from '../assets';
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className="w-full flex justify-center items-center flex-col">
+        <nav className="flex justify-between items-center w-full mb-10 pt-3">
+            <img src={logo} alt="sumz_logo"
+            className="w-28 object-contain"/>
+
+            <button type="button" 
+            onClick={() => window.open('https://github.com/huangr0867')}
+            className="black_btn">
+                GitHub
+            </button>
+        </nav>
+
+        <h1 className="head_text">
+            Summarize Articles with <br className="max-md:hidden"/>
+            <span className="orange_gradient">OpenAI GPT-4</span>
+        </h1>
+        <h2 className="desc">
+            Simplify your reading of websites and articles with webSnap
+        </h2>
+    </header>
   )
 }
 
